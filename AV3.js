93eaa599668ad4766a87eb586b34008862813849
Wolleebob <guilherme.schultze.galvao@escola.pr.gov.br>
2026-03-09 11:37:27 -0300

    Add files via upload

---
+++ AV3.js
@@ -0,0 +1,42 @@
+/*
+Imagine que você abriu uma loja:
+1) Crie o nome de 8 produtos e armazene em uma lista;
+2) Crie outra lista para armazenar o preço de cada produto.
+3) Crie uma função para mostrar o estoque, mostrando o nome da cada produto e o seu preço.
+4) Crie uma função que adiciona um novo produto (e consequentemente um novo preço)
+5) Crie uma função que remove um produto da lista recebendo seu índice.
+6) EXECUTE AS FUNÇÕES CRIADAS NA SEGUINTE ORDEM:
+    - mostra estoque
+    - adiciona produto
+    - mostra estoque
+    - remove um elemento pelo índice
+    - mostra estoque
+*/
+
+const roupas = ["Camiseta", "Calça", "Blusa", "Moletom", "Jaqueta", "Tênis", "Sapato", "Shorts"]
+const preço =  [    16    ,    18  ,    19  ,     13   ,     22   ,    25  ,    32   ,    9    ]
+
+function mostrarProdutos(){
+    let contador = 0 
+    while(contador < roupas.length){ 
+        console.log(roupas[contador] + " - R$" + preço[contador])
+        contador = contador + 1 
+    }
+    console.log("---------------------------------")
+}
+
+mostrarProdutos()
+function adcionarProdutos(nome,valor){
+    roupas.push(nome)
+    preço.push(valor)
+}
+adcionarProdutos("Carteira", 35)
+mostrarProdutos()
+
+function removerProdutos(indice){
+    roupas.splice(indice,1)
+    preço.splice(indice,1)
+}
+removerProdutos(4)
+mostrarProdutos()
+
